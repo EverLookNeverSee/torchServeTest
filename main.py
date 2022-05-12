9f958f960d5fa2ec5e22f0c0611f2622b1b040af
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-05-12 13:11:20 +0430

    Creating model class

--- main.py
+++ main.py
@@ -5,3 +5,17 @@
 
 import torch
 import torchvision
+
+
+class ConvBnReluModel(torch.nn.Module):
+    def __init__(self):
+        super(ConvBnReluModel, self).__init__()
+        self.conv = torch.nn.Conv2d(3, 5, 3, bias=False).to(dtype=torch.float)
+        self.bn = torch.nn.BatchNorm2d(5).to(dtype=torch.float)
+        self.relu = torch.nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        return x
