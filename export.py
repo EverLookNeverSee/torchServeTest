65a24c1e5337310a6538260ba2416cafe7aff35f
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-05-16 13:23:23 +0430

    Implementing export section

--- export.py
+++ export.py
@@ -5,3 +5,10 @@
 
 import torch
 from torchvision.models import resnet34
+
+
+model = resnet34(pretrained=True)
+example_input = torch.rand(1, 3, 224, 224)
+model.eval()
+traced_model = torch.jit.trace(model, example_input)
+traced_model.save("./resnet34,pt")
