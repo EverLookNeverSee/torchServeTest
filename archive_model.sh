e867ce9d7e4dcabb2d5667f591966cb88d250fda
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-05-16 13:46:52 +0430

    Implementing model archiver shell script

---
+++ archive_model.sh
@@ -0,0 +1,6 @@
+torch-model-archiver --model-name resnet34 \
+--version 0.0.1 \
+--serialized-file resnet34.pt \
+--extra-files ./index_to_name.json,./handler.py \
+--handler handler.py  \
+--export-path model_store -f
