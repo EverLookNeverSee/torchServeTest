a28f4d22c5f6bc65285392e776d5d10ceb7ab107
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-05-16 15:06:45 +0430

    Fixing serve.sh

--- serve.sh
+++ serve.sh
@@ -2,5 +2,5 @@
 
 sudo docker run --rm -it \
 -p 3000:8080 -p 3001:8081 \
--v $(pwd)/model_store:/home/model_server/model_store pytorch/torchserve:0.1-cpu \
-torchserve --start --model-store model_store --models resnet34=resnet34.mar
+-v $(pwd)/model-store:/home/model_server/model-store pytorch/torchserve:0.1-cpu \
+torchserve --start --model-store model-store --models resnet34=resnet34.mar
