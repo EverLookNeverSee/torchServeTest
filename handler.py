5cbe945c2e3c5d8e8b801d92fa7840c3c2f7c0ca
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-05-16 13:16:30 +0430

    Importing relevant packages

--- handler.py
+++ handler.py
@@ -1,4 +1,13 @@
 """
-    Main Script
+    Main Handler Script
     @Author: Milad Sadeghi DM - EverLookNeverSee@GitHub
 """
+
+
+import io
+import torch
+from PIL import Image
+from requests import request
+import torch.nn.functional as F
+from torchvision import transforms
+from ts.torch_handler.base_handler import BaseHandler
